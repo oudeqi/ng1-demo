3e00f0fe5df0ff157edbea3126c074778d567f26
oudeqi <496997374@qq.com>
2017-03-25 14:38:01 +0800

    ***************

--- src/component/app/app-template.js
+++ src/component/app/app-template.js
@@ -4,10 +4,6 @@
 
 angular.module('appTemplate', []).run(['$templateCache', function($templateCache) {
 
-  $templateCache.put('./login/login.html', '<div>登录xxxzxzxczxczczxczxczxc</div>');
-
-  $templateCache.put('./login/xx.html', '<div></div>');
-
 }]);
 
 })();
